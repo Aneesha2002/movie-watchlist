85494b77a6886c2071ec3676f47a5e66b4f79528
Aneesha <aneeshadsouza2002@gmail.com>
2025-11-27 19:38:50 +0530

    DEBUG

--- watchlist/settings.py
+++ watchlist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m^h4=jh4f!9&+mibhd(z#-wxtm(+%!rrmbjb@_*mu2c4-h3cm2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
